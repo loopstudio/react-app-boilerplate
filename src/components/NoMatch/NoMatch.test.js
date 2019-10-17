5877dc7b70a483b4b6b8aa0f3d58b7f52f29d678
Micaela <Wolman>
2019-10-17 13:36:34 -0300

    Re order imports

--- src/components/NoMatch/NoMatch.test.js
+++ src/components/NoMatch/NoMatch.test.js
@@ -1,6 +1,7 @@
-import '@testing-library/jest-dom/extend-expect'
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import NoMatch from './index';
 
 test('shows a "404 Not found" message', () => {
