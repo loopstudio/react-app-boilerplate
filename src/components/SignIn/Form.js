dbdab048433fd6b78da67ba0a0b091b9000a3ecf
Valentina Franchi <vfranchi@loopstud.io>
2019-11-12 17:06:55 -0300

    Add fomik as a form helper

--- src/components/SignIn/Form.js
+++ src/components/SignIn/Form.js
@@ -1,51 +1,66 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { signIn } from 'actions/auth';
 
 const Form = () => {
-  const [credentials, setCredentials] = useState({
-    email: '',
-    password: '',
-  });
+  const dispatch = useDispatch();
+
   const [formStatus, setFormStatus] = useState({
     hasErrors: false,
     isLoading: false,
   });
 
-  const dispatch = useDispatch();
+  const SignInSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Required'),
+    password: Yup.string().required('Required'),
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setFormStatus({ hasErrors: false, isLoading: true });
-    dispatch(signIn(credentials)).catch(() => {
-      setFormStatus({ hasErrors: true, isLoading: false });
-      setCredentials({ ...credentials, password: '' });
-    });
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: SignInSchema,
+    onSubmit: (values) => {
+      setFormStatus({ ...formStatus, isLoading: true });
+      dispatch(signIn(values)).catch(() => {
+        setFormStatus({ hasErrors: true, isLoading: false });
+      });
+    },
+  });
 
-  const handleFieldChange = (event) => {
-    const { value, name } = event.target;
-    setCredentials({ ...credentials, [name]: value });
-  };
-
+  const {
+    values: { email, password },
+    handleChange,
+    handleSubmit,
+    errors,
+    touched,
+  } = formik;
   const { isLoading, hasErrors } = formStatus;
-  const { email, password } = credentials;
 
   return (
     <form onSubmit={handleSubmit}>
       {isLoading && <p>Loading...</p>}
       {hasErrors && <p>There was an error.</p>}
       <label>Email</label>
-      <input name="email" value={email} onChange={handleFieldChange} />
+      <input name="email" value={email} onChange={handleChange} />
+      {touched.email && errors.email}
       <label>Password</label>
       <input
         name="password"
         value={password}
         type="password"
-        onChange={handleFieldChange}
+        onChange={handleChange}
       />
-      <button type="submit">Sign in</button>
+      {touched.password && errors.password}
+      <button type="submit" disabled={isLoading}>
+        Sign in
+      </button>
     </form>
   );
 };
--- src/components/SignUp/Form.js
+++ src/components/SignUp/Form.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { signUp } from 'actions/auth';
 
 const Form = () => {
-  const [user, setUser] = useState({
-    email: '',
-    firstName: '',
-    lastName: '',
-    password: '',
-  });
   const [formStatus, setFormStatus] = useState({
     hasErrors: false,
     isLoading: false,
@@ -17,40 +13,63 @@ const Form = () => {
 
   const dispatch = useDispatch();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setFormStatus({ hasErrors: false, isLoading: true });
-    dispatch(signUp(user)).catch(() => {
-      setFormStatus({ hasErrors: true, isLoading: false });
-      setUser({ ...user, password: '' });
-    });
-  };
+  const SignUpSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email')
+      .required('Required'),
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    password: Yup.string()
+      .min(8, 'Password too short, minimum length is 8 characters')
+      .required('Required'),
+  });
 
-  const handleFieldChange = (event) => {
-    const { value, name } = event.target;
-    setUser({ ...user, [name]: value });
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+    },
+    validationSchema: SignUpSchema,
+    onSubmit: (values) => {
+      setFormStatus({ hasErrors: false, isLoading: true });
+      dispatch(signUp(values)).catch(() => {
+        setFormStatus({ hasErrors: true, isLoading: false });
+      });
+    },
+  });
 
+  const {
+    values: { email, firstName, lastName, password },
+    handleChange,
+    handleSubmit,
+    errors,
+    touched,
+  } = formik;
   const { isLoading, hasErrors } = formStatus;
-  const { email, firstName, lastName, password } = user;
 
   return (
     <form onSubmit={handleSubmit}>
       {isLoading && <p>Loading...</p>}
       {hasErrors && <p>There was an error.</p>}
       <label>Email</label>
-      <input name="email" value={email} onChange={handleFieldChange} />
+      <input name="email" value={email} onChange={handleChange} />
+      {touched.email && errors.email}
       <label>First Name</label>
-      <input name="firstName" value={firstName} onChange={handleFieldChange} />
+      <input name="firstName" value={firstName} onChange={handleChange} />
+      {touched.firstName && errors.firstName}
       <label>Last Name</label>
-      <input name="lastName" value={lastName} onChange={handleFieldChange} />
+      <input name="lastName" value={lastName} onChange={handleChange} />
+      {touched.lastName && errors.lastName}
       <label>Password</label>
       <input
         name="password"
         value={password}
         type="password"
-        onChange={handleFieldChange}
+        onChange={handleChange}
       />
+      {touched.password && errors.password}
       <button type="submit">Sign up</button>
     </form>
   );
