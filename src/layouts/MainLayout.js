9d380c14fa529b1ed574b2a29738e91544bb1e3d
Donato Aguirre <donatoaguirre24@gmail.com>
2019-12-13 14:47:24 -0300

    Update src/layouts/MainLayout.js
    Co-Authored-By: Mauricio Coniglio <mconiglio.782@gmail.com>

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MainLayout = ({ children }) => {
-  return <>{children}</>;
+  return children;
 };
 
 MainLayout.propTypes = {
