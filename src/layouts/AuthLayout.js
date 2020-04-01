0c622477bfea8af5360a9deb66581660c5bfc7c0
Micaela <Wolman>
2020-04-01 18:28:27 -0300

    Fix issues introduced when fixing merge conflicts

--- src/layouts/AuthLayout.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const AuthLayout = ({ children }) => children;
-
-AuthLayout.propTypes = {
-  children: PropTypes.element.isRequired,
-};
-
-export default AuthLayout;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 import Home from 'components/Home';
 import MainLayout from 'layouts/MainLayout';
-import Navbar from 'components/Navbar';
 
 const HomePage = () => (
   <MainLayout>
-    <Navbar />
     <Home />
   </MainLayout>
 );
