63decfd6ae143d4ab0ecb92441ef8ea5ee49d8b0
mwolman <33032571+mwolman@users.noreply.github.com>
2020-04-01 19:39:12 -0300

    Fix issues introduced when fixing merge conflicts (#140)
    Co-authored-by: Micaela <Wolman>

--- src/layouts/MainLayout.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const MainLayout = ({ children }) => children;
-
-MainLayout.propTypes = {
-  children: PropTypes.element.isRequired,
-};
-
-export default MainLayout;
