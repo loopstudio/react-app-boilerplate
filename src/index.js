e4cd03e66f05b01debf7b722043277d6f764135f
mollerjorge <jmoller2106@gmail.com>
2020-05-28 19:26:45 -0300

    Start styles migration to emotion

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React, { StrictMode } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'emotion-theming';
 
 import App from 'components/App';
 import { persistor, store } from 'store';
 import * as serviceWorker from 'serviceWorker';
+import theme from './theme';
 
 if (process.env.NODE_ENV !== 'production') {
   import('react-axe').then((axe) => axe.default(React, ReactDOM, 1000));
@@ -15,7 +17,9 @@ render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </StrictMode>,
