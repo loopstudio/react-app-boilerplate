b94efa9b60140dbd33bceeeaf797586996a5b945
mwolman <33032571+mwolman@users.noreply.github.com>
2020-06-11 15:25:47 -0300

    Fix axe warnings + UI tweak (#254)
    * Fix axe warnings
    
    * Change style of "Forgot Password?" link
    
    Co-authored-by: Micaela <Wolman>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import style from './Home.module.scss';
 
-const Home = () => <div className={style.content}>Hello World</div>;
+const Home = () => <h1 className={style.content}>Hello World</h1>;
 
 export default Home;
