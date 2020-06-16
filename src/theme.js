f7aaca5967f0d0a82448777e3d1ad9a481cf1e20
Rodrigo Minetti <51714443+Rminetti@users.noreply.github.com>
2020-06-16 14:01:39 -0300

    nav bar styles migration (#257)

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,49 @@
 export default {
+  borderRadius: {
+    none: '0',
+    sm: '0.125rem',
+    base: '0.25rem',
+    md: '0.375rem',
+    lg: '0.5rem',
+    full: '9999px',
+  },
+  boxShadow: {
+    darkMd:
+      '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    darkLg:
+      '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    darkXl:
+      '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    none: 'none',
+  },
   color: {
     black: '#000000',
     white: '#FFFFFF',
     gray200: '#EDF2F7',
+    gray300: '#E2E8F0',
+    gray600: '#718096',
     gray700: '#4A5568',
+    indigo400: '#7F9CF5',
+    indigo500: '#667EEA',
+  },
+  fontSize: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    xxl: '1.5rem',
+    x3l: '1.875rem',
+    x4l: '2.25rem',
+    x5l: '3rem',
+    x6l: '4rem',
+  },
+  letterSpacing: {
+    tighter: '-0.05rem',
+    tight: '-0.025rem',
+    normal: '0',
+    wide: '0.025rem',
+    wider: '0.05rem',
+    widest: '0.1rem',
   },
 };
