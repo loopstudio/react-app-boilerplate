0ffd604f30b2c6c0318a5a43afae4a35480d7ed6
Facundo Petre <facu1p3@gmail.com>
2020-06-17 21:29:40 -0300

    fix(nagigation/Logo.js): use client side navigation on Logo.js

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Logo.module.scss';
 
@@ -6,9 +7,9 @@ const PRODUCT_NAME = 'LoopStudio';
 
 const Logo = () => (
   <div className={styles.logo}>
-    <a href="/" className={styles.brand}>
+    <Link to="/" className={styles.brand}>
       {PRODUCT_NAME}
-    </a>
+    </Link>
   </div>
 );
 
