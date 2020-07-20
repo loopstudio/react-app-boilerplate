f131fd4573d41b7755877614e6bd8d964ce9eb62
mwolman <33032571+mwolman@users.noreply.github.com>
2020-07-20 16:52:01 -0300

    Migrate NoMatch styles to emotion, fix axe and styling issues (#336)

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
 
-import styles from './NoMatch.module.scss';
+import { Content, Wrapper, Title, Legend, GoBackHome } from './NoMatch.styles';
 
 const NoMatch = () => (
-  <div className={styles.content}>
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>
+  <Content>
+    <Wrapper>
+      <Title>
         <FormattedMessage id="common.pageNotFoundTitle" />
-      </h1>
-      <p className={styles.legend}>
+      </Title>
+      <Legend>
         <FormattedMessage id="common.pageNotFoundLegend" />
-      </p>
-      <Link to="/" className={styles.goBackHome}>
+      </Legend>
+      <GoBackHome to="/">
         <FormattedMessage id="common.goBackHome" />
-      </Link>
-    </div>
-  </div>
+      </GoBackHome>
+    </Wrapper>
+  </Content>
 );
 
 export default NoMatch;
