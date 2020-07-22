6fff81ec7935750a983db467fb29f13ee1fedbb1
emmaielle <moira.lasserre@gmail.com>
2020-07-22 12:05:41 -0300

    Fixes setError call according to react hook form updates

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -2,19 +2,17 @@ import { capitalize } from './string';
 
 export function handleErrors({ attributesErrors, errors }, setError) {
   if (errors?.length) {
-    setError('general', 'custom', errors[0]);
+    setError('general', { message: errors[0] });
   }
 
   if (attributesErrors && Object.keys(attributesErrors).length) {
     const filteredErrors = Object.keys(attributesErrors).filter(
       (fieldName) => attributesErrors[fieldName].length
     );
-
-    setError(
-      filteredErrors.map((fieldName) => ({
+    filteredErrors.forEach((fieldName) =>
+      setError(fieldName, {
         message: capitalize(attributesErrors[fieldName][0]),
-        name: fieldName,
-      }))
+      })
     );
   }
 }
