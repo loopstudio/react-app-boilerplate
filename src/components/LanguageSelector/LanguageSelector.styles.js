ed3e81b14e19658ba8c9986db2a5672f95de93fe
Moira Lasserre <emmaielle@users.noreply.github.com>
2020-07-22 10:09:06 -0300

    Moved Language selector styles to emotion (#344)

---
+++ src/components/LanguageSelector/LanguageSelector.styles.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Select = styled.select`
+  bottom: 10px;
+  height: 45px;
+  outline: 0;
+  position: absolute;
+  right: 10px;
+  width: 100px;
+  z-index: 10;
+`;
