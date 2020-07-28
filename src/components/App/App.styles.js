18e72adbd74c04b888693badf13ee0c5709237e1
Loop <loop@Loops-MacBook-Pro.local>
2020-07-28 13:33:42 -0300

    Delete funcion in App styles

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -1,9 +1,9 @@
 import { css } from '@emotion/core';
 
-export const loadingStyles = () => css`
+export const loadingStyles = css`
   height: 100vh;
 `;
 
-export const globalStyles = () => css`
+export const globalStyles = css`
   font-family: 'Open Sans', sans-serif;
 `;
