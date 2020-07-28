a7b6ff991aa827c0c6d0fa4f43b38ff65e530537
Jorge Moller <jmoller2106@gmail.com>
2020-07-28 10:37:06 -0300

    Migrate loading styles to emotion (#347)

---
+++ src/components/Loading/Loading.styles.js
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { css, keyframes } from '@emotion/core';
+
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const Spinner = styled.div`
+  height: 2.5rem;
+  margin: auto;
+  position: relative;
+  width: 2.5rem;
+`;
+
+const doubleBounceStyles = ({ theme }) => css`
+  animation: ${bounce} 2s infinite ease-in-out;
+  background: ${theme.color.indigo600};
+  border-radius: 50%;
+  height: 100%;
+  left: 0;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
+
+export const DoubleBounce = styled.div`
+  ${doubleBounceStyles}
+`;
+
+export const DoubleBounceWithDelay = styled.div`
+  ${doubleBounceStyles}
+  animation-delay: -1s;
+`;
+
+const bounce = keyframes`
+  0%,
+  100% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+`;
