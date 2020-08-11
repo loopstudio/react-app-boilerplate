a333333569bf788cd09b4192c07bd00f90cdfc36
Rodrigo Minetti <51714443+Rminetti@users.noreply.github.com>
2020-08-11 16:37:05 -0300

    Fixes wrong import (#378)
    Co-authored-by: Rodrigo <rodrigo@LSUY-009.local>

--- src/pages/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ForgotPassword from 'components/ForgotPassword/ForgotPassword';
+import ForgotPassword from 'components/ForgotPassword';
 import MainLayout from 'layouts/MainLayout';
 
 const ForgotPasswordPage = () => (
