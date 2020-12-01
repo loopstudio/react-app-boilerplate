24c6d14ea8b771d5854eaaaf114074d41b10dfa9
Tiago-Zarzavidjian <60147387+Tiago-Zarzavidjian@users.noreply.github.com>
2020-12-01 14:27:52 -0300

    Create test for src/index.js (#469)

---
+++ src/index.test.js
@@ -0,0 +1,39 @@
+import React, { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from '@emotion/react';
+
+import App from 'components/App';
+import configureStore from 'store';
+import httpClient, { applyMiddlewares } from 'services/httpClient';
+import theme from 'theme';
+
+const { store, persistor } = configureStore();
+applyMiddlewares(httpClient, store);
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+
+describe('index.js', () => {
+  it('renders without crashing', () => {
+    const root = document.createElement('div');
+    root.id = 'root';
+    document.body.appendChild(root);
+
+    // eslint-disable-next-line global-require
+    require('./index.js');
+
+    render(
+      <StrictMode>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <ThemeProvider theme={theme}>
+              <App />
+            </ThemeProvider>
+          </PersistGate>
+        </Provider>
+      </StrictMode>,
+      root
+    );
+  });
+});
