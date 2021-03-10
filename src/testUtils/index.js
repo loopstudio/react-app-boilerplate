bdf70995f8de3d00e9f12d295999876ca909c3ad
Fede <fede@seedwalk.net>
2021-03-10 09:29:47 -0300

    Updated testUtils Wrapper to have AuthProvider (#498)
    Co-authored-by: Federico Caramelli <fede@loopstudio.dev>

--- src/testUtils/index.js
+++ src/testUtils/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import flatten from 'flat';
 import PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
-// import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
@@ -10,27 +9,24 @@ import { ThemeProvider } from '@emotion/react';
 import theme from 'theme';
 
 import AppLocale from 'features/app/locales';
-// import configureStore from 'store';
-import httpClient from 'features/app/services/httpClient'; // applyMiddlewares,
+import httpClient from 'features/app/services/httpClient';
 import { GuestLocaleProvider } from 'features/app/context/guestLocale';
+import { AuthProvider } from 'features/auth';
 
 const renderWithProviders = (
   ui,
   { state = {}, history = createMemoryHistory(), ...options } = {}
 ) => {
-  // const { store } = configureStore({ initialState: state, persist: false });
-  // applyMiddlewares(httpClient, store);
-
   const Wrapper = ({ children }) => (
-    // <Provider store={store}>
     <GuestLocaleProvider>
       <IntlProvider locale="en" messages={flatten(AppLocale.en.messages)}>
         <ThemeProvider theme={theme}>
-          <Router history={history}>{children}</Router>
+          <AuthProvider httpClient={httpClient}>
+            <Router history={history}>{children}</Router>
+          </AuthProvider>
         </ThemeProvider>
       </IntlProvider>
     </GuestLocaleProvider>
-    // </Provider>
   );
 
   Wrapper.propTypes = {
