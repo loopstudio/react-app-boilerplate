f0189fd38b8de56d763b78c51286bccb6ef9bc35
gonchihernandez <80699310+gonchihernandez@users.noreply.github.com>
2021-04-12 14:03:40 -0300

    Fix fontawesome warnings (#507)
    * Fix fontawesome warnings
    
    * Fix fontawesome warnings
    
    * Update src/setupTests.js
    
    Co-authored-by: Valentina Franchi <valef2894@gmail.com>
    
    Co-authored-by: Mauricio Coniglio <mconiglio.782@gmail.com>
    Co-authored-by: Valentina Franchi <valef2894@gmail.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,12 @@
 import httpAdapter from 'axios/lib/adapters/http';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 import httpClient from 'features/app/services/httpClient';
+import icons from 'assets/icons';
 
 beforeAll(() => {
   httpClient.defaults.adapter = httpAdapter;
+  library.add(icons);
 });
 
 beforeEach(() => {
