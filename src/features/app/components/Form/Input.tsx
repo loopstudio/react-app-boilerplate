3dcff3a7b3179ed94f14414a8d90e6b914f3db2b
Tiago-Zarzavidjian <tzarzavidjian@loopstudio.dev>
2021-07-28 14:44:39 -0300

    Refactor custom Form styles using styled from emotion

--- src/features/app/components/Form/Input.tsx
+++ src/features/app/components/Form/Input.tsx
@@ -6,6 +6,9 @@ import InputLabel from './InputLabel';
 import { Error, Input, InputContainer } from './Form.styles';
 
 interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  name: string;
+  label: string;
 }
 
 const FormInput = ({ id, name, label, ...rest }: FormInputProps) => {
